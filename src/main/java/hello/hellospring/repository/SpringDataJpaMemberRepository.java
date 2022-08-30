31cc208e2a08c39f4d8eaa63fb6e95a02ce7dc5b
xabxabit <sabee_castle@naver.com>
2022-08-30 23:40:39 +0900

    스프링 데이터 JPA

--- src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
+++ src/main/java/hello/hellospring/repository/SpringDataJpaMemberRepository.java
@@ -7,5 +7,6 @@ import java.util.Optional;
 // 스프링 데이터 JPA 회원 리포지토리
 // JpaRepository 인터페이스(스프링 데이터 JPA)와 MemberRepository 인터페이스를 상속한 인터페이스 생성
 public interface SpringDataJpaMemberRepository extends JpaRepository<Member, Long>, MemberRepository {
+
     Optional<Member> findByName(String name);
 }
